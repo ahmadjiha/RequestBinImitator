ccb4c4d6ee97362f71632fddc7a9e416c976c43a
Ahmad Jiha <ahmad.j.jiha@gmail.com>
2022-06-01 19:23:04 -0700

    Add route to view a request bin

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World</h1>');
+});
+
+app.get('/bins/views/:id', (request, response) => {
+  const binId = request.params.id;
+  response.sendFile('./views/1.html', { root: __dirname });
 });
 
 const PORT = 3007;

a84e0704afa89d46cfd19482c5d7552f48d5e870
Ahmad Jiha <ahmad.j.jiha@gmail.com>
2022-06-02 17:56:00 -0700

    Add route to create a new bin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Index.js - Contains express app
 
+const db = require('./modules/request-bin-db');
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -12,6 +13,25 @@ app.get('/bins/views/:id', (request, response) => {
   const binId = request.params.id;
   response.sendFile('./views/1.html', { root: __dirname });
 });
+
+// Create a new request bin 
+app.post('/bins/views', (request, response) => {
+  const dateCreated = new Date();
+  const url = `tempurl: ${dateCreated}`; // Come back implement this
+
+  db.one('INSERT INTO bins(url, date_created, date_last_used) VALUES($1, $2, $3) RETURNING id', [url, dateCreated, dateCreated])
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      // error handling here
+      console.log(error);
+    });
+  
+  response.send('<h1>Bin Added</h1>') // return a 302 redirect with path
+});
+
+
 
 const PORT = 3007;
 

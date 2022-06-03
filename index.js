b37f1d3db33798eceaff2751a8c4fdaba66bc482
Ahmad Jiha <ahmad.j.jiha@gmail.com>
2022-06-02 18:11:22 -0700

    Update routes: 1) view a bin, 2) Create a new bin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Index.js - Contains express app
 
+const crypto = require('crypto');
 const db = require('./modules/request-bin-db');
 const express = require('express');
 const app = express();
@@ -9,29 +10,27 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World</h1>');
 });
 
-app.get('/bins/views/:id', (request, response) => {
-  const binId = request.params.id;
-  response.sendFile('./views/1.html', { root: __dirname });
+app.get('/bins/views/:uri', (request, response) => {
+  const binUri = request.params.uri;
+  response.sendFile(`./views/${binUri}.html`, { root: __dirname });
 });
 
 // Create a new request bin 
 app.post('/bins/views', (request, response) => {
   const dateCreated = new Date();
-  const url = `tempurl: ${dateCreated}`; // Come back implement this
+  const uriHash = crypto.randomBytes(20).toString('hex');
+  const uri = `${uriHash}`; // Come back implement this
 
-  db.one('INSERT INTO bins(url, date_created, date_last_used) VALUES($1, $2, $3) RETURNING id', [url, dateCreated, dateCreated])
+  db.one('INSERT INTO bins(url, date_created, date_last_used) VALUES($1, $2, $3) RETURNING url', [uri, dateCreated, dateCreated])
     .then(data => {
       console.log(data);
+      response.redirect();
     })
     .catch(error => {
       // error handling here
       console.log(error);
-    });
-  
-  response.send('<h1>Bin Added</h1>') // return a 302 redirect with path
+    });  
 });
-
-
 
 const PORT = 3007;
 
